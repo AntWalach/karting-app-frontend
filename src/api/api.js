3bece9d77ad08af7f507606276625acda2b5b903
bszarlowicz <b.szarlowicz@gmail.com>
2025-05-12 00:34:26 +0200

    default api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:3000/api';  // dostosuj do swojego backendu!
-const API_URL = 'http://0.0.0.0:3001/api';  // dostosuj do swojego backendu!
+const API_URL = 'http://localhost:3000/api';  // dostosuj do swojego backendu!
+// const API_URL = 'http://0.0.0.0:3001/api';  // dostosuj do swojego backendu!
 
 const api = axios.create({
   baseURL: API_URL
